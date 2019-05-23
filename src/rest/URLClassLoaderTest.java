85042d512d728b0f5a3bce67152a354a131b0508
rmamaral <rm.amaral@campus.fct.unl.pt>
2019-05-23 19:34:25 +0100

    With process test

--- src/rest/URLClassLoaderTest.java
+++ src/rest/URLClassLoaderTest.java
@@ -1,5 +1,6 @@
 package rest;
 
+import java.io.File;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 import java.net.URL;
@@ -18,21 +19,29 @@ public class URLClassLoaderTest {
     public static void main(String[] args) throws Exception {
 
         // Getting the jar URL which contains target class
-        URL[] classLoaderUrls = new URL[]{new URL("file:///home/ashraf/Desktop/simple-bean-1.0.jar")};
+        URL[] classLoaderUrls = new URL[]{new URL("file://C:\\Users\\ricar\\OneDrive\\Área de Trabalho\\CSD\\SecureReplicatedBank\\src\\rest\\Hello.jar")};
+
+        ProcessBuilder pb = new ProcessBuilder("java", "-jar",  "file://C:\\Users\\ricar\\OneDrive\\Área de Trabalho\\CSD\\SecureReplicatedBank\\src\\rest\\Hello.jar");
+        Process p = pb.start();
+        p.destroy();
+        while(p.isAlive()){
+            System.out.println("ola");
+        }
+
 
         // Create a new URLClassLoader
-        URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls);
+        //URLClassLoader urlClassLoader = new URLClassLoader(classLoaderUrls);
 
         // Load the target class
-        Class<?> beanClass = urlClassLoader.loadClass("com.jcg.Bean");
+        //Class<?> helloClass = urlClassLoader.loadClass("rest.Hello");
 
         // Create a new instance from the loaded class
-        Constructor<?> constructor = beanClass.getConstructor();
-        Object beanObj = constructor.newInstance();
+        //Constructor<?> constructor = helloClass.getConstructor();
+        //Object beanObj = constructor.newInstance();
 
         // Getting a method from the loaded class and invoke it
-        Method method = beanClass.getMethod("sayHello");
-        method.invoke(beanObj);
+        //Method method = helloClass.getMethod("sayHello");
+        //method.invoke(beanObj);
 
     }
 
